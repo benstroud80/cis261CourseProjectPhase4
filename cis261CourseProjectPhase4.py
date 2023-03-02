ba5c1548267e13bfd293f9cd6e43a17fe162f7af
strayer_practice@outlook.com <strayer_practice@outlook.com>
2023-03-02 14:28:00 -0800

    phase four attempt 2

--- cis261CourseProjectPhase4.py
+++ cis261CourseProjectPhase4.py
@@ -1,41 +1,42 @@
-#
-#
-#
 from datetime import datetime
-################################################################################
+
 def CreateUsers():
     print('##### Create users, passwords, and roles #####')
-    ########## Open the file user.txt in append mode and assign to UserFile
-     
+    # Open the file user.txt in append mode and assign to UserFile
+    UserFile = open("Users.txt", "a")
     while True:
-        ########## Write the line of code that will call function GetUserName and assign the return value to username
-        
+        # Call function GetUserName and assign the return value to username
+        username = GetUserName()
         if (username.upper() == "END"):
             break
-        ########## Write the line of code that will call function GetUserPassword and assign the return value to userpwd
-        
-        ########## Write the line of code that will call function GetUserRole() and assign the return value to userrole
-        
+        # Call function GetUserPassword and assign the return value to userpwd
+        userpwd = GetUserPassword()
+        # Call function GetUserRole() and assign the return value to userrole
+        userrole = GetUserRole()
         UserDetail = username + "|" + userpwd + "|" + userrole + "\n"  
         UserFile.write(UserDetail)
     # close file to save data
-    ########## Write the line of code that will close the file UserFile
-       
+    # Close the file UserFile
+    UserFile.close()
     
-
 def GetUserName():
-    ##### write the code to enter the username or End and return username 
-   
+    # Enter the username or End and return username
+    username = input("Enter user name or 'End': ")
+    return username
 
 def GetUserPassword():
-    ##### write the code to enter the pwd and return pwd
-
+    # Enter the password and return pwd
+    userpwd = input("Enter user password: ")
+    return userpwd
 
 def GetUserRole():
-     userrole = input("Enter role (Admin or User): ")
-     while True:
-         ####### write the if statement that validates that Admin or User has been entered. If true, return userrole.  If false, re-input userrole
-
+    # Enter the user role (Admin or User) and validate it
+    while True:
+        userrole = input("Enter role (Admin or User): ")
+        if userrole.upper() == "ADMIN" or userrole.upper() == "USER":
+            return userrole.upper()
+        else:
+            print("Invalid role. Please enter either Admin or User.")
 
 def printuserinfo():
     UserFile = open("Users.txt","r")
@@ -46,49 +47,50 @@ def printuserinfo():
         UserDetail = UserDetail.replace("\n", "") #remove carriage return from end of line
         UserList = UserDetail.split("|")
         username = UserList[0]
-        userpassword = UserList[1]
+        userpwd = UserList[1]
         userrole = UserList[2]
-        print("User Name: ", username, " Password: ", userpassword, " Role: ", userrole)
-
-############################################################################################
-
+        print("User Name: ", username, " Password: ", userpwd, " Role: ", userrole)
+  
 def Login():
-        # read login information and store in a list
-    ########## Write the line of code that will open the file Users.txt in read mode
-    
-    
+    # read login information and store in a list
+    # Open the file Users.txt in read mode
+    UserFile = open("Users.txt", "r")
     UserName = input("Enter User Name: ")
     UserRole = "None"
     while True:
-       ########## Write the line of code that will read a line from UserFile and assign it to UserDetail
-             
-       if not UserDetail:
-           return UserRole, UserName
-       ########## Write the line of code that will replace the carriage return in UserDetail
-       
-       ########## Write the line of code that will split UserDetail on the pipe delimiter (|) and assign it to UserList
-                  
-       if UserName == UserList[0]:
+        # Read a line from UserFile and assign it to UserDetail
+        UserDetail = UserFile.readline()
+        if not UserDetail:
+            break
+        # Replace the carriage return in UserDetail
+        UserDetail = UserDetail.replace("\n", "")
+        # Split UserDetail on the pipe delimiter (|) and assign it to UserList
+        UserList = UserDetail.split("|")
+        if UserName == UserList[0]:
             UserRole = UserList[2]  # user is valid, return role
             return UserRole, UserName
-    return UserRole, UserName
-#########################################################################################
+     
 def GetEmpName():
-    empname = input("Enter employee name: ")
+    empname = input ("Enter employee name or END to end: ")
     return empname
+
 def GetDatesWorked():
     fromdate = input("Enter Start Date (mm/dd/yyyy: ")
     todate = input("Enter End Date (mm/dd/yyyy: ")
     return fromdate, todate
+
 def GetHoursWorked():
-    hours = float(input('Enter amount of hours worked:  '))
+    hours = float(input ("Enter amount of hours worked: "))
     return hours
+
 def GetHourlyRate():
     hourlyrate = float(input ("Enter hourly rate: "))
     return hourlyrate
+
 def GetTaxRate():
     taxrate = float(input ("Enter tax rate: "))
     return taxrate
+
 def CalcTaxAndNetPay(hours, hourlyrate, taxrate):
     grosspay = hours * hourlyrate
     incometax = grosspay * taxrate
@@ -120,17 +122,17 @@ def printinfo(DetailsPrinted):
         EmpDetail = EmpDetail.replace("\n", "") #remove carriage return from end of line
         EmpList = EmpDetail.split("|")
         fromdate = EmpList[0]
-        if (str(rundate).upper() != "ALL"):
+        if (str(rundate).upper() !="ALL"):
             checkdate = datetime.strptime(fromdate, "%m/%d/%Y")
-            if (checkdate < rundate):
-                continue        
+            if(checkdate < rundate):
+                continue
         todate = EmpList[1]
         empname = EmpList[2]
         hours = float(EmpList[3])
-        hourlyrate  = float(EmpList[4])
+        hourlyrate = float(EmpList[4])
         taxrate = float(EmpList[5])
         grosspay, incometax, netpay = CalcTaxAndNetPay(hours, hourlyrate, taxrate)
-        print(fromdate, todate, empname, f"{hours:,.2f}",  f"{hourlyrate:,.2f}", f"{grosspay:,.2f}",  f"{taxrate:,.1%}",  f"{incometax:,.2f}",  f"{netpay:,.2f}")
+        print(fromdate, todate, empname, f"{hours:,.2f}", f"hourlyrate:,.2f", f"{grosspay:,.2f}"), f"{taxrate:,.1%}", f"{incometax:,.2f}", f"{netpay:,.2f}")
         TotEmployees += 1
         TotHours += hours
         TotGrossPay += grosspay
@@ -146,35 +148,32 @@ def printinfo(DetailsPrinted):
         PrintTotals (EmpTotals)
     else:
         print("No detail information to print")
+
 def PrintTotals(EmpTotals):    
-    print()
-    print(f'Total Number Of Employees: {EmpTotals["TotEmp"]}')
-    print(f'Total Hours Worked: {EmpTotals["TotHrs"]:,.2f}')
-    print(f'Total Gross Pay: {EmpTotals["TotGrossPay"]:,.2f}')
-    print(f'Total Income Tax:  {EmpTotals["TotTax"]:,.2f}')
-    print(f'Total Net Pay: {EmpTotals["TotNetPay"]:,.2f}')
+      print()
+      print(f'Total Number Of Employees: {EmpTotals["TotEmp"]}')
+      print(f'Total Hours Worked: {EmpTotals["TotHrs"]:,.2f}')
+      print(f'Total Gross Pay: {EmpTotals["TotGrossPay"]:,.2f}')
+      print(f'Total Income Tax:  {EmpTotals["TotTax"]:,.2f}')
+      print(f'Total Net Pay: {EmpTotals["TotNetPay"]:,.2f}')
 
 
-   
-
+                                                      
 if __name__ == "__main__":
     ##################################################
     ########## Write the line of code to call the method CreateUsers
+    CreateUsers()
     
     print()
     print("##### Data Entry #####")
-    ########## Write the line of code to assign UserRole and UserName to the function Login
-     
+    UserRole, UserName = Login()
     DetailsPrinted = False  ###
     EmpTotals = {} ###
-    ########## Write the if statement that will check to see if UserRole is equal to NONE (NOTE: code will show red error lines until this line is written)
-    
+    if (UserRole == "NONE"):
         print(UserName," is invalid.")
     else:
-    # only admin users can enter data
-        ##### write the if statement that will check to see if the UserRole is equal to ADMIN (NOTE: code will show red error lines until this line is written)
-        
-   
+        # only admin users can enter data
+        if (UserRole == "ADMIN"):
             EmpFile = open("Employees.txt", "a+")                
             while True:
                 empname = GetEmpName()
@@ -186,7 +185,6 @@ if __name__ == "__main__":
                 taxrate = GetTaxRate()
                 EmpDetail = fromdate + "|" + todate  + "|" + empname  + "|" + str(hours)  + "|" + str(hourlyrate)  + "|" + str(taxrate) + "\n"  
                 EmpFile.write(EmpDetail)
-        # close file to save data
+            # close file to save data
             EmpFile.close()    
-        printinfo(DetailsPrinted)
-
+    printinfo(DetailsPrinted)
